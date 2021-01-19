6fd5a3995afbf4b3d1322652ce6772ecab259078
Alessio Ferrato <75256972+musica-maestro@users.noreply.github.com>
2021-01-19 18:55:36 +0100

    create file for bin testing

---
+++ examples/bin_Potree.jl
@@ -0,0 +1,23 @@
+using Potree
+using FileManager 
+using Visualization
+using BenchmarkTools
+using Profile
+
+println("Load done")
+
+# potree source folder
+potree = "C:/Users/Alessio/Documents/potreeDirectory/pointclouds/scale_1.7/" # replace this path with local potree directory
+
+println("loading trie...")
+Ptrie = Potree.potree2trie(potree)
+println("searching files...")
+PMall_files = Potree.get_all_values(Ptrie)
+println("creating point cloud...")
+PPC = Potree.las2pointcloud(PMall_files...)
+
+ GL.VIEW(
+      [
+      Visualization.points_color_from_rgb(PPC.coordinates,PPC.rgbs)
+      ]
+  ) 
