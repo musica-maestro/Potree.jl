ea775eb8dfd45968a5c2ca6b605c80860c0dfdf7
Alessio Ferrato <75256972+musica-maestro@users.noreply.github.com>
2021-01-24 19:15:56 +0100

    Create Benchmark_Potree_FileManager.jl

---
+++ test/Benchmark_Potree_FileManager.jl
@@ -0,0 +1,54 @@
+using Potree
+using FileManager 
+using Visualization
+using BenchmarkTools
+using Profile
+
+# potree source folder
+# big files preferable
+#potree = "C:/Users/Alessio/Documents/potreeDirectory/pointclouds/CAVA" # replace this path with local potree directory
+potree = "C:/Users/Alessio/.julia/dev/Potree/potree/stairsLAS(v1.7)"
+
+
+println("==============================================================================")
+println("@btime for FileManager: potree2triee, get_all_values, las2pointcloud")
+FMtrie = @btime FileManager.potree2trie($potree)
+FMall_files = @btime FileManager.get_all_values($FMtrie)
+FMPC = @btime FileManager.las2pointcloud($FMall_files...)
+println("==============================================================================")
+println("==============================================================================")
+println("@btime for Potree: potree2triee, get_all_values, las2pointcloud")
+Ptrie = @btime Potree.potree2trie($potree)
+PMall_files = @btime Potree.get_all_values($Ptrie)
+PPC = @btime Potree.las2pointcloud($PMall_files...)
+println("==============================================================================")
+
+# Section used to check if there is any difference 
+# during the output of the 2 packages
+
+# println("==============================================================================")
+# println("==============================================================================")
+# println("FMall_files == PMall_files (Next line should be true)")
+# println(FMall_files == PMall_files)
+# println("==============================================================================")
+# println("size(FMPC.coordinates) == size(PPC.coordinates) (Next line should be true)")
+# println(size(FMPC.coordinates) == size(PPC.coordinates))
+# println("==============================================================================")
+# println("==============================================================================")
+
+# Section used to check if there is any difference 
+# during the Visualization of the results
+
+# # point cloud
+# GL.VIEW(
+#     [
+#     Visualization.points_color_from_rgb(PPC.coordinates,PPC.rgbs)
+#     ]
+# ) 
+
+# # point cloud
+# GL.VIEW(
+#     [
+#     Visualization.points_color_from_rgb(FMPC.coordinates,FMPC.rgbs)
+#     ]
+# ) 
