392456e8c83ad7825a226033d147224335103994
Alessio Ferrato <75256972+musica-maestro@users.noreply.github.com>
2021-01-29 16:58:18 +0100

    bin & las example

--- examples/bin_las_potree_example.jl
+++ examples/bin_las_potree_example.jl
@@ -2,39 +2,24 @@ using Potree
 using Visualization
 using BenchmarkTools
 using Profile
-
 using DataStructures
-
-println("Load done")
 
 # potree source folder
 potreeBin = "C:/Users/Alessio/.julia/dev/Potree/potree/stairsBIN(v1.7)" # replace this path with local potree directory
 potreeLas = "C:/Users/Alessio/.julia/dev/Potree/potree/stairsLAS(v1.7)" # replace this path with local potree directory
 
-println("creating point cloud for bin...")
+# potree source folder
+println("bin loading...")
 Vtot = Potree.bin2pointcloud(potreeBin)
-
+println("las loading...")
 Vtot2 = Potree.source2pc(potreeLas, -1)
 
+println("Same points?")
 println(Vtot == Vtot2.coordinates)
 
-# goodPoints = 0
+println("Same number of points?")
+println(size(Vtot)==size(Vtot2.coordinates))
 
-# for v in Vtot
-#     for vinner in Vtot2.coordinates
-#         if(v==vinner)
-#             splice(Vtot2.coordinates, vinner) 
-#             global goodPoints += 1
-#         end
-#     end
-# end
-
-println("Number of good points")
-println(goodPoints)
-println("Number of 1")
-println(size(Vtot))
-println("Number of 2")
-println(size(Vtot2.coordinates))
  GL.VIEW(
       [
       Visualization.points(Vtot)
